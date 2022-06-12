a65527b3506fbfc9fc5af76bef4d77057e4721ba
Amer Markisic <amer3107@gmail.com>
2022-06-13 00:06:30 +0200

    Added Trouble Which key and VimWiki

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -10,6 +10,9 @@ require('bufferline').setup({
       max_prefix_length = 25, -- prefix used when a buffer is deduplicated
       tab_size = 25,
       diagnostics ="nvim_lsp",
+      themable = true,
+      show_tab_indicators = true,
+      enforce_regular_tabs = false,
       -- indicator_icon = '▎',
       -- buffer_close_icon = '',
       -- modified_icon = '●',
@@ -20,7 +23,7 @@ require('bufferline').setup({
       -- buffer_close_icon = '',
       show_buffer_close_icons = false,
       show_close_icon = false,
-      offsets = {{filetype = "NvimTree", text="File Explorer" }},
+      offsets = {{filetype = "NvimTree", text=""}},
       diagnostics_indicator = function(count, level, diagnostics_dict, context)
         local icon = level:match("error") and " " or " "
         return " " .. icon .. count
