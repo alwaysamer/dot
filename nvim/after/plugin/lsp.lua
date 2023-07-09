e779a15289aebb2988a5158d0d73a6cdda9c2189
Amer Markisic <amer@markisic.io>
2023-07-09 17:30:29 +0200

    Disabled vim warning in Lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,6 +10,16 @@ lsp.ensure_installed({
 	'rust_analyzer',
 })
 
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
