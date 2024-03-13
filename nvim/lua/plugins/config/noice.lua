4e95de62adbe1bc300aff6cf87240f4cb804e58b
Amer Markisic <amer3107@gmail.com>
2024-03-13 12:37:15 +0100

    edited noice

--- nvim/lua/plugins/config/noice.lua
+++ nvim/lua/plugins/config/noice.lua
@@ -22,9 +22,12 @@ return
                     ["cmp.entry.get_documentation"] = true,
                 },
             },
+            cmdline = {
+                view = "cmdline",
+            },
             presets = {
                 bottom_search = true,
-                command_palette = true,
+                command_palette = false,
                 long_message_to_split = true,
                 inc_rename = false,
                 lsp_doc_border = false,
