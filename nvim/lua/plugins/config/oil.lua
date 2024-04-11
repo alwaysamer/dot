a66dd9ebf2c89dd95d3abee6c051969282bccffd
Amer Markisic <amer3107@gmail.com>
2024-04-11 22:37:30 +0200

    added oil.nvim

---
+++ nvim/lua/plugins/config/oil.lua
@@ -0,0 +1,36 @@
+return {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            default_file_explorer = true,
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            },
+            float = {
+                padding = 2,
+                max_width = 70,
+                max_height = 0,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                },
+            },
+        })
+    end
+}
