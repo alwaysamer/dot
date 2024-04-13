be0fd4e474187318583c9c2732587b82f0075f49
Amer Markisic <amer3107@gmail.com>
2024-04-13 23:40:03 +0200

    removed obsidian edited dashboard and made keymaps silent

--- nvim/lua/plugins/config/dashboard.lua
+++ nvim/lua/plugins/config/dashboard.lua
@@ -1,28 +1,42 @@
 return {
     'goolord/alpha-nvim',
-    config = function ()
+    config = function()
         local alpha = require("alpha")
         local dashboard = require("alpha.themes.dashboard")
 
         dashboard.section.header.val = {
-           "  █████╗ ███╗   ███╗███████╗██████╗ ",
-           " ██╔══██╗████╗ ████║██╔════╝██╔══██╗",
-           " ███████║██╔████╔██║█████╗  ██████╔╝",
-           " ██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗",
-           " ██║  ██║██║ ╚═╝ ██║███████╗██║  ██║",
-           " ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝",
+            "  █████╗ ███╗   ███╗███████╗██████╗ ",
+            " ██╔══██╗████╗ ████║██╔════╝██╔══██╗",
+            " ███████║██╔████╔██║█████╗  ██████╔╝",
+            " ██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗",
+            " ██║  ██║██║ ╚═╝ ██║███████╗██║  ██║",
+            " ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝",
         }
 
         dashboard.section.buttons.val = {
-            dashboard.button( "n", "  > New file" , ":ene <BAR> startinsert <CR>"),
-            dashboard.button( "r", "  > Recent", ":Telescope oldfiles<CR>"),
-            dashboard.button( "p", "  > Projects"   , ":Telescope projects<CR>"),
-            dashboard.button( "d", "  > Journal", ":ObsidianToday<CR>"),
-            dashboard.button( "s", "  > Settings" , ":e $MYVIMRC<CR>"),
-            dashboard.button( "q", "󰩈  > Quit NVIM", ":qa<CR>"),
+            dashboard.button("n", "  > New file", ":silent ene <BAR> startinsert <CR>"),
+            dashboard.button("r", "  > Recent", ":silent Telescope oldfiles<CR>"),
+            dashboard.button("p", "  > Projects", ":silent Telescope projects<CR>"),
+            dashboard.button("s", "  > Settings", ":silent e $MYVIMRC<CR>"),
+            dashboard.button("q", "󰩈  > Quit NVIM", ":silent qa<CR>"),
         }
 
         alpha.setup(dashboard.opts)
+
+        vim.api.nvim_create_autocmd("User", {
+            callback = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime * 100) / 100
+                dashboard.section.footer.val = "󱐌 Lazy-loaded "
+                    .. stats.loaded
+                    .. "/"
+                    .. stats.count
+                    .. " plugins in "
+                    .. ms
+                    .. "ms"
+                pcall(vim.cmd.AlphaRedraw)
+            end,
+        })
 
         vim.cmd([[
             autocmd FileType alpha setlocal nofoldenable
