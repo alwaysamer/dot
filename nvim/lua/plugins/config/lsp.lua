685668602d29db01f06e0d3a2ab071ad1a3ba034
alwaysamer <amer3107@gmail.com>
2024-05-27 21:31:16 +0200

    Fixed oil FloatTitle background, extracted scala, removed lsp zero and setup styling for cmp and lsp functions

--- nvim/lua/plugins/config/lsp.lua
+++ nvim/lua/plugins/config/lsp.lua
@@ -1,32 +1,6 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    'neovim/nvim-lspconfig',
     dependencies = {
-        { 'neovim/nvim-lspconfig' },
-        -- {
-        --     "scalameta/nvim-metals",
-        --     ft = { "scala", "sbt", "java" },
-        --     dependencies = {
-        --         "nvim-lua/plenary.nvim",
-        --     },
-        --     opts = function()
-        --         local metals_config = require("metals").bare_config()
-        --         metals_config.on_attach = function(client, bufnr)
-        --         end
-        --
-        --         return metals_config
-        --     end,
-        --     config = function(self, metals_config)
-        --         local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-        --         vim.api.nvim_create_autocmd("FileType", {
-        --             pattern = self.ft,
-        --             callback = function()
-        --                 require("metals").initialize_or_attach(metals_config)
-        --             end,
-        --             group = nvim_metals_group,
-        --         })
-        --     end
-        -- },
         { 'williamboman/mason.nvim', },
         { 'williamboman/mason-lspconfig.nvim' },
         {
@@ -38,6 +12,8 @@ return {
         { 'onsails/lspkind.nvim' },
         { 'hrsh7th/nvim-cmp' },
         { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-cmdline' },
         {
             'L3MON4D3/LuaSnip',
             build = "make install_jsregexp",
@@ -48,8 +24,12 @@ return {
         require("neodev").setup({
             library = { types = true, plugins = { "neotest" } },
         })
-        local lsp = require('lsp-zero')
-        lsp.preset("recommended")
+        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local default_setup = function(server)
+            require('lspconfig')[server].setup({
+                capabilities = lsp_capabilities,
+            })
+        end
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
@@ -62,7 +42,25 @@ return {
                 'lua_ls',
             },
             handlers = {
-                lsp.default_setup,
+                default_setup,
+                gopls = function()
+                    require('lspconfig').gopls.setup({
+                        capabilities = lsp_capabilities,
+                        settings = {
+                            gopls = {
+                                hints = {
+                                    assignVariableTypes = true,
+                                    compositeLiteralFields = true,
+                                    compositeLiteralTypes = true,
+                                    constantValues = true,
+                                    functionTypeParameters = true,
+                                    parameterNames = true,
+                                    rangeVariableTypes = true,
+                                }
+                            },
+                        },
+                    })
+                end,
             },
         })
 
@@ -71,9 +69,6 @@ return {
 
         local cmp = require('cmp')
         cmp.setup({
-            completion = {
-                completeopt = 'menu,menuone,noinsert',
-            },
             formatting = {
                 format = kind.cmp_format({
                     mode = 'symbol_text',
@@ -119,16 +114,42 @@ return {
             preselect = cmp.PreselectMode.None,
             window = {
                 completion = {
-                    border = "rounded",
+                    border = "single",
                     scrollbar = false,
                 },
                 documentation = {
-                    border = "rounded",
+                    border = "single",
                     scrollbar = true,
                     maxwidth = 80,
                     maxheight = 40,
                 },
             },
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+            })
+        })
+
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' }
+                    }
+                }
+            })
         })
 
         local cmp_autopairs = require('nvim-autopairs.completion.cmp')
@@ -136,23 +157,48 @@ return {
             'confirm_done',
             cmp_autopairs.on_confirm_done()
         )
-        lsp.on_attach(function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<C-j>", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "<C-k>", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
 
-        lsp.setup()
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "<C-j>", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "<C-k>", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("n", "<leader>vi",
+                    function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts)
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            end
+        })
+
+        local _border = "single"
+
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+            vim.lsp.handlers.hover, {
+                max_width = 80,
+                maxheight = 40,
+                border = _border
+            }
+        )
+
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help, {
+                maxwidth = 80,
+                maxheight = 40,
+                border = _border
+            }
+        )
 
         vim.diagnostic.config({
+
             underline = true,
+            float = { border = _border },
             virtual_text = false,
             signs = true,
         })
--- nvim/lua/plugins/config/oil.lua
+++ nvim/lua/plugins/config/oil.lua
@@ -22,16 +22,16 @@ return {
                 ["g."] = "actions.toggle_hidden",
                 ["g\\"] = "actions.toggle_trash",
             },
-            -- float = {
-            --     padding = 2,
-            --     max_width = 70,
-            --     max_height = 0,
-            --     border = "rounded",
-            --     win_options = {
-            --         winblend = 0,
-            --     },
-            -- },
-        })
-    vim.keymap.set("n", "<leader>pv", ":Oil<CR>", { silent = true })
+            float = {
+                padding = 2,
+                max_width = 70,
+                max_height = 0,
+                border = "single",
+                win_options = {
+                    winblend = 0,
+            },
+        }
+    })
+        vim.keymap.set("n", "<leader>pv", ":Oil --float<CR>", { silent = true })
     end
 }
