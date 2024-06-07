49bfd14323c299e408c58d97a10e2b0a71367817
alwaysamer <amer3107@gmail.com>
2024-06-07 11:26:15 +0200

    Set Lsp Diagnostic-Signs

--- nvim/lua/plugins/config/lsp.lua
+++ nvim/lua/plugins/config/lsp.lua
@@ -236,6 +236,12 @@ return {
             virtual_text = false,
             signs = true,
         })
+
+        local signs = { Error = " ", Warn = " ", Hint = "󰝶 ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
         vim.lsp.inlay_hint.enable(true)
     end
 }
