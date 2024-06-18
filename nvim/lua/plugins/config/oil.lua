dbeec6ea6b06cc32e0eb03989bbddb948ed95747
alwaysamer <amer3107@gmail.com>
2024-06-18 19:43:57 +0200

    Changed some oil settings and added floatborder color

--- nvim/lua/plugins/config/oil.lua
+++ nvim/lua/plugins/config/oil.lua
@@ -4,6 +4,18 @@ return {
     config = function()
         require("oil").setup({
             default_file_explorer = true,
+            delete_to_trash = true,
+            skip_confirm_for_simple_edits = true,
+            view_options = {
+                show_hidden = true,
+                natural_order = true,
+                is_always_hidden = function(name, _)
+                    return name == '..' or name == '.git'
+                end
+            },
+            lsp_file_methods = {
+                autosave_changes = true,
+            },
             float = {
                 padding = 2,
                 max_width = 50,
@@ -11,9 +23,10 @@ return {
                 border = "single",
                 win_options = {
                     winblend = 0,
-            },
-        }
-    })
+                },
+            }
+        })
         vim.keymap.set("n", "<leader>pv", ":Oil --float<CR>", { silent = true })
+        vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff" })
     end
 }
