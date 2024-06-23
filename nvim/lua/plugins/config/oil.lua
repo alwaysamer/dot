dec38a4b84d226e252a1871f18a808b94cb85cc7
alwaysamer <amer3107@gmail.com>
2024-06-23 21:57:25 +0200

    added some zsh aliases and added preview for oil float

--- nvim/lua/plugins/config/oil.lua
+++ nvim/lua/plugins/config/oil.lua
@@ -18,13 +18,32 @@ return {
             },
             float = {
                 padding = 2,
-                max_width = 50,
+                max_width = 100,
                 max_height = 0,
+                preview_split = "right",
                 border = "single",
                 win_options = {
                     winblend = 0,
                 },
-            }
+            },
+            preview = {
+                max_width = { 100, 0.8 },
+                -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+                min_width = { 80, 0.4 },
+                -- optionally define an integer/float for the exact width of the preview window
+                width = { 50 },
+                -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+                -- min_height and max_height can be a single value or a list of mixed integer/float types.
+                -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+                max_height = 0.9,
+                -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+                min_height = { 5, 0.1 },
+                border = "single",
+                win_options = {
+                    winblend = 0,
+                },
+                update_on_cursor_moved = true,
+            },
         })
         vim.keymap.set("n", "<leader>pv", ":Oil --float<CR>", { silent = true })
         vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff" })
