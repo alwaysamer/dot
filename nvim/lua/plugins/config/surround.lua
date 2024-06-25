090a59c052ace298e887991e8f86df0537e69e2b
alwaysamer <amer3107@gmail.com>
2024-06-25 17:23:25 +0200

    edited surround keymaps

--- nvim/lua/plugins/config/surround.lua
+++ nvim/lua/plugins/config/surround.lua
@@ -2,6 +2,20 @@ return {
     'echasnovski/mini.surround',
     version = '*',
     config = function()
-        require('mini.surround').setup()
+        require('mini.surround').setup(
+            {
+                mappings = {
+                    add = '<c-s>',
+                    delete = 'sd',
+                    find = '',
+                    find_left = '',
+                    highlight = '',
+                    replace = 'sr',
+                    update_n_lines = '',
+                    suffix_last = '',
+                    suffix_next = '',
+                },
+            }
+        )
     end,
 }
