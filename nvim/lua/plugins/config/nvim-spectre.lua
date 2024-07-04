c69cd22410f0e660d099e7c07d53b494d5ae29fc
alwaysamer <amer3107@gmail.com>
2024-07-04 22:04:21 +0200

    edited spectre keymaps

--- nvim/lua/plugins/config/nvim-spectre.lua
+++ nvim/lua/plugins/config/nvim-spectre.lua
@@ -3,21 +3,6 @@ return {
     cmd = "Spectre",
     keys = {
         {
-            "<leader>sf",
-            function()
-                require("spectre").open_file_search({ select_word = true })
-            end,
-            desc = "Spectre Search in File",
-        },
-        {
-            "<leader>s",
-            function()
-                require("spectre").open_visual({ select_word = true })
-            end,
-            desc = "Spectre Search in File",
-        },
-
-        {
             "<leader>S",
             function()
                 require("spectre").toggle()
