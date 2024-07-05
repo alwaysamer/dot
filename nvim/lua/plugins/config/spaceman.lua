63371d618fc8a00c0d2646b817e409524aee25ca
alwaysamer <amer3107@gmail.com>
2024-07-05 23:39:03 +0200

    spaceman auto open oil and disable sessions

--- nvim/lua/plugins/config/spaceman.lua
+++ nvim/lua/plugins/config/spaceman.lua
@@ -13,16 +13,16 @@ return {
             sort_by_recent = true,
             use_default_keymaps = false,
             use_default_hooks = true,
-            use_sessions = true,
+            use_sessions = false,
             rename_function = nil,
             adapter = "vim-ui",
             hooks = {
                 before_move = nil,
-                after_move = nil,
+                after_move = function()
+                    require('oil').open(vim.fn.getcwd())
+                end,
             },
-            telescope_opts = nil,
             data_path = vim.fn.stdpath("data") .. "/spaceman_data.json",
-            sessions_path = vim.fn.stdpath("data") .. "/sessions",
         })
     end,
 }
