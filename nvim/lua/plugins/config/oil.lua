4594d2b48ee691d401ee1431b9a3fdaef38cb1d2
alwaysamer <amer3107@gmail.com>
2024-07-06 16:00:38 +0200

    edited oil

--- nvim/lua/plugins/config/oil.lua
+++ nvim/lua/plugins/config/oil.lua
@@ -5,6 +5,7 @@ return {
         require("oil").setup({
             default_file_explorer = true,
             skip_confirm_for_simple_edits = true,
+            delete_to_trash = true,
             view_options = {
                 show_hidden = true,
                 natural_order = true,
@@ -17,13 +18,31 @@ return {
             },
             float = {
                 padding = 2,
-                max_width = 100,
+                max_width = 50,
                 max_height = 0,
                 preview_split = "right",
                 border = "single",
                 win_options = {
                     winblend = 0,
                 },
+            },
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+                ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+                ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+                ["<C-p>"] = false,
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
             },
             preview = {
                 max_width = { 50, 0.5 },
