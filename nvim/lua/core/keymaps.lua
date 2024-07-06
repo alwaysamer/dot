ec5e78540702b565272661e8a7bcac396b2cea56
alwaysamer <amer3107@gmail.com>
2024-07-06 15:01:28 +0200

    added codesnap and removed obsidian

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -6,7 +6,6 @@ vim.keymap.set("n", "<C-i>", ":bp<CR>", { silent = true, desc = "Buffer Previous
 vim.keymap.set("n", "<C-o>", ":bn<CR>", { silent = true, desc = "Buffer Next" })
 vim.keymap.set("n", "n", "nzzzv", { silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { silent = true })
-vim.keymap.set("n", "<leader>od", "<cmd>vsplit ~/vaults/neovim/start.md<CR>", { silent = true, desc = "Notes Open" })
 vim.keymap.set("i", "<C-c>", "<ESC>", { silent = true })
 vim.keymap.set("n", "Q", "<nop>", { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Line Move Down" })
