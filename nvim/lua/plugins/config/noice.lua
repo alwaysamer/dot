3cedb6f9707f4fd2bf8c9038a1c865baeb89abd9
alwaysamer <amer3107@gmail.com>
2024-07-07 01:15:14 +0200

    finally fixed notify appearance

--- nvim/lua/plugins/config/noice.lua
+++ nvim/lua/plugins/config/noice.lua
@@ -2,9 +2,25 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     dependencies = {
+        "rcarriga/nvim-notify",
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("notify").setup({
+            level = vim.log.levels.TRACE,
+            render = "wrapped-compact",
+            on_open = function(win)
+                local config = vim.api.nvim_win_get_config(win)
+                config.border = "single"
+                vim.api.nvim_win_set_config(win, config)
+            end
+        })
+        vim.api.nvim_set_hl(0, "NotifyERRORBorder", { link = "FloatBorder" })
+        vim.api.nvim_set_hl(0, "NotifyWARNBorder", { link = "FloatBorder" })
+        vim.api.nvim_set_hl(0, "NotifyINFOBorder", { link = "FloatBorder" })
+        vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { link = "FloatBorder" })
+        vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { link = "FloatBorder" })
+
         require("noice").setup({
             cmdline = {
                 enabled = true,
@@ -26,7 +42,8 @@ return {
                 },
             },
             notify = {
-                enabled = false,
+                enabled = true,
+                view = "notify",
             },
             lsp = {
                 progress = {
@@ -45,8 +62,6 @@ return {
                 },
                 message = {
                     enabled = false,
-                    view = "notify",
-                    opts = {},
                 },
             },
             health = {
