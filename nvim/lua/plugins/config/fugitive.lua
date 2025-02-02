5facadc273cab56240a7fb782c446e3678f0f058
alwaysamer <amer3107@gmail.com>
2025-02-02 12:24:05 +0100

    neovim: edited fugitive bindings

--- nvim/lua/plugins/config/fugitive.lua
+++ nvim/lua/plugins/config/fugitive.lua
@@ -1,9 +1,6 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gP", ":Git push<CR>",
-            { silent = true, desc = "Git Push" })
-        vim.keymap.set("n", "<leader>gp", ":Git pull<CR>",
-            { silent = true, desc = "Git pull" })
+        vim.keymap.set("n", "<leader>gg",":0G<CR>", { silent = true, desc = "Git Diff" })
     end
 }
