00b310b3d1afbd3be7a612a729939bd83ab5c13a
alwaysamer <amer3107@gmail.com>
2025-03-01 15:52:45 +0100

    neovim: refactored blink.cmp

--- nvim/lua/plugins/config/blink-cmp.lua
+++ nvim/lua/plugins/config/blink-cmp.lua
@@ -6,14 +6,19 @@ return {
             'L3MON4D3/LuaSnip', version = 'v2.*'
         },
         {
-            "giuxtaposition/blink-cmp-copilot",
+            "fang2hou/blink-copilot",
+            opts = {
+                max_completions = 1,
+                max_attempts = 2,
+                kind_hl = "Normal",
+            }
         },
     },
     version = '*',
     config = function()
         require("blink.cmp").setup({
             sources = {
-                default = { "lazydev","lsp", "path", "snippets", "buffer", "copilot" },
+                default = { "lazydev", "lsp", "path", "snippets", "buffer", "copilot" },
                 providers = {
                     lazydev = {
                         name = "LazyDev",
@@ -22,25 +27,14 @@ return {
                     },
                     copilot = {
                         name = "copilot",
-                        module = "blink-cmp-copilot",
+                        module = "blink-copilot",
                         score_offset = 100,
                         async = true,
-                        transform_items = function(_, items)
-                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-                            local kind_idx = #CompletionItemKind + 1
-                            CompletionItemKind[kind_idx] = "Copilot"
-                            for _, item in ipairs(items) do
-                                item.kind = kind_idx
-                            end
-                            return items
-                        end,
-
                     },
                 },
             },
             appearance = {
                 kind_icons = {
-                    Copilot = "",
                     Text = '󰉿',
                     Method = '󰊕',
                     Function = '󰊕',
@@ -74,6 +68,7 @@ return {
                 },
             },
             completion = {
+                list = { selection = { preselect = true, auto_insert = false } },
                 menu = {
                     scrollbar = false,
                     border = {
