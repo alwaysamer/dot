e5e77b96cde0a21df1c026a0f414e84998b0fbdd
alwaysamer <amer3107@gmail.com>
2025-03-03 00:21:41 +0100

    neovim: refactored lsp and lazydev + removed auto-session

--- nvim/lua/plugins/config/lazydev.lua
+++ nvim/lua/plugins/config/lazydev.lua
@@ -1,11 +1,4 @@
 return {
-  {
     "folke/lazydev.nvim",
     ft = "lua",
-    opts = {
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
 }
--- nvim/lua/plugins/config/lsp.lua
+++ nvim/lua/plugins/config/lsp.lua
@@ -19,28 +19,6 @@ return {
             },
             handlers = {
                 default_setup,
-                lua_ls = function()
-                    require("lspconfig").lua_ls.setup {
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                                workspace = {
-                                    library = {
-                                        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                                        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-                                        [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-                                        [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
-                                        [vim.fn.expand "${3rd}/love2d/library"] = true,
-                                    },
-                                    maxPreload = 100000,
-                                    preloadFileSize = 10000,
-                                },
-                            },
-                        },
-                    }
-                end,
             },
         })
 
