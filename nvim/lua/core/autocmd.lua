ba357bb264a3ad261ffef69300376e9e37514718
alwaysamer <amer3107@gmail.com>
2025-03-09 20:49:45 +0100

    Revert "neovim: refactor autocmd"
    This reverts commit 3e3227171edf554700ea80fb2fbc8d0170d561c6.

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -3,3 +3,9 @@ vim.api.nvim_create_autocmd("BufLeave", {
     command = "silent! wall",
     group = vim.api.nvim_create_augroup("AutoSaveOnBufLeave", { clear = true }),
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+    group = vim.api.nvim_create_augroup("TrimWhitespace", { clear = true }),
+})
