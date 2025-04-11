72427fed6974e1ee9c954936e3e16caf79a65ad5
alwaysamer <amer3107@gmail.com>
2025-04-11 11:50:40 +0200

    neovim: added notify

---
+++ nvim/lua/plugins/config/notify.lua
@@ -0,0 +1,9 @@
+return {
+    'rcarriga/nvim-notify',
+    config = function ()
+        require('notify').setup({
+            render = 'compact',
+        })
+        vim.notify = require("notify")
+    end
+}
