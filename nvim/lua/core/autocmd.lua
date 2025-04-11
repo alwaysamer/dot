0dbc1b1c2b9f436c8802dd0894da2891e5e765e4
alwaysamer <amer3107@gmail.com>
2025-04-11 11:50:12 +0200

    neovim: added autocmd do delete global marks

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -3,3 +3,8 @@ vim.api.nvim_create_autocmd("BufLeave", {
     command = "silent! wall",
     group = vim.api.nvim_create_augroup("AutoSaveOnBufLeave", { clear = true }),
 })
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = function()
+    vim.cmd("delmarks A-Z")
+  end,
+})
